2565ffd7bcc3fba60101c2faab7668847c45e0a9
therealrealRYAN <rr697224@ohio.edu>
2025-09-05 17:56:14 -0400

    lab2 program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Reagan Ryan>
+ *        Date: <9/5/25>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) { "Hello Git!"
+
+    /*add code*/
+    return 0;
+}// main
